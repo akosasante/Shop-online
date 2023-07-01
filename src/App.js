d2e3e4aee02f357ad7a0c4096aa43d41234ac85d
Akosua <9094098+akosasante@users.noreply.github.com>
2023-07-01 18:06:48 +0000

    add router into main section of the page, to render login/logout/register components when URL changes, and show shopping page on main route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css'
+import React from 'react'
 import Header from './Components/HeaderContent'
 import Shopping from './pages/Shopping'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Logout from './pages/Logout'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
@@ -11,9 +17,20 @@ function App() {
         </nav>
       </header>
 
-    <main>
-      <Shopping />
-    </main>
+      <main>
+        <Router>
+          <Routes>
+            <Route
+              exact
+              path='/login'
+              element={<Login afterLogin={() => {}} />}
+            />
+            <Route exact path='/register' element={<Register />} />
+            <Route exact path='/logout' element={<Logout />} />
+            <Route path='*' element={<Shopping />} />
+          </Routes>
+        </Router>
+      </main>
     </div>
   )
 }
